3a6e1f6ef27a63d4fe9c3e9664912f1d8f0cd2f7
Simon Rowe <srowe@pivotal.io>
2018-10-01 10:19:12 +0100

    Commit demo code

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # mutual-auth-demo
-Demo using spring boot 2 for mutual (X.509) authentication
+Demo using spring boot 2 for mutual (X.509) authentication.
+
+### Generating Client Certificates
+
+1. Generate the client keystore: ``` keytool -genkey -alias bmc -keyalg RSA -keystore keyStore.jks -keysize 2048```
+2. Generate a CA cert and key: ```openssl req -new -x509 -keyout ca-key -out ca-cert.crt ```
+3. Generate a CSR (make sure you set the CN to client *as this demo will depend on that*): ```keytool -keystore keystore.jks -alias csr -certreq -file csr ```
+4. Sign the cert: ```openssl x509 -req -CA ca-cert.crt -CAkey ca-key.key -in csr -out cert-signed.crt -days 365 -CAcreateserial -passin pass:yourpass ```
+5. Import CA cert into keystore: ``` keytool -keystore keystore.jks -alias CARoot -import -file ca-cert.crt```
+6. Import Signed cert into keystore: ```keytool -keystore keystore.jks -alias client -import -file cert-signed.crt ```
+7. Export the private key: 
+
+* ```keytool -importkeystore -srckeystore keystore.jks -destkeystore keystore.p12 -deststoretype PKCS12 ```
+* ```openssl pkcs12 -in keystore.p12  -nodes -nocerts -out client.key ```
+
+### Importing CA cert into truststore
+1. Generate truststore: ```keytool -keystore truststore.jks -alias cacert -import -file ca-cert.crt ```
+
+
+## To run the app
+```mvn package```
+
+```java -jar target/mutual-auth-server-0.0.1-SNAPSHOT.jar```
+
+## Test in postman
+* Import the cert
+![](img/setup-certificate.png)
+
+* Sample HTTPS Get Request
+![](img/run.png)
+
+
+
---
+++ src/main/java/com/example/mutualauthserver/MutualAuthServerApplication.java
@@ -0,0 +1,108 @@
+package com.example.mutualauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RestController
+public class MutualAuthServerApplication extends WebSecurityConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutualAuthServerApplication.class, args);
+    }
+
+
+    /**
+     * This will configure the X509AuthenticationFilter, and will authenticate any requests where the provided certificate
+     * is included in the trust store, or the certificate has been signed by a CA which is included in the truststore
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(userDetailsService());
+    }
+
+    /**
+     * Ignore security for the /headers endpoint.
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/headers");
+    }
+
+
+    /**
+     * Creates a user service, which will provide authorization to users with CN=client
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return (String username) -> {
+            if (username.equals("client")) {
+                return new User(username, "",
+                        AuthorityUtils
+                                .commaSeparatedStringToAuthorityList("ROLE_USER"));
+            }
+            return null;
+        };
+    }
+
+
+    /**
+     * Spits out information about the client certificate used to authenticate with this app
+     * @param principal
+     * @return
+     */
+    @GetMapping("/")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String user(Principal principal) {
+        Authentication auth = (Authentication) principal;
+        return String.valueOf(auth.getCredentials());
+    }
+
+    /**
+     * This http endpoint spits out all of the headers
+     * When running on PCF and if the go routers are configured to pass the cert through to the
+     * app instance, we should see the header X-Forwarded-Client-Cert.
+     * @param request
+     * @return
+     */
+    @GetMapping("/headers")
+    public Map<String, String> headers(HttpServletRequest request) {
+        LinkedHashMap<String, String> headers = new LinkedHashMap<>();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.put(headerName, request.getHeader(headerName));
+        }
+        return headers;
+    }
+
+}
